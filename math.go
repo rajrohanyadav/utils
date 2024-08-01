7db7d72bedccb1ba1cc68838ec494a177446936d
Rohan Yadav <rajrohanyadav@gmail.com>
2024-08-01 19:27:04 +0530

    use generics for min and max functions

--- math.go
+++ math.go
@@ -1,17 +1,31 @@
-package utils 
+package utils
 
-// TODO: Use generics here
-func MaxInt(a, b int) int {
-  if a >= b {
-    return a
-  }
-  return b
+type comparableNumbers interface {
+  int|int8|int16|int32|int64|float32|float64  
 }
 
-// TODO: use generics here
-func MinInt(a, b int) int {
-  if a <= b {
-    return a
+func Max[T comparableNumbers](st ...T) T {
+  if len(st) == 0 {
+    return 0
   }
-  return b
+  max := st[0]
+  for _, e := range st {
+    if (e > max) {
+      max = e
+    }
+  }
+  return max
+}
+
+func Min[T comparableNumbers](st ...T) T {
+  if len(st) == 0 {
+    return 0
+  }
+  min := st[0]
+  for _, e := range st {
+    if (e < min) {
+      min = e
+    }
+  }
+  return min
 }
--- math_test.go
+++ math_test.go
@@ -6,27 +6,62 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMaxInt(t *testing.T) {
-	tests := []struct {
-		name     string
-		input    []int
+func TestMax(t *testing.T) {
+	tests := map[string]struct{
+		input []int
 		expected int
 	}{
-		{
-			name:     "first number larger",
-			input:    []int{5, 4},
-			expected: 5,
+		"no number": {
+			input: []int{},
+			expected: 0,
 		},
-		{
-			name:     "second number larger",
-			input:    []int{3, 4},
-			expected: 4,
+		"one number": {
+			input: []int{1},
+			expected: 1,
+		},
+		"two numbers": {
+			input: []int{1,2},
+			expected: 2,
+		},
+		"more than two numbers": {
+			input: []int{234, 523, 51, 532, 51, 533, 11, 0, 97},
+			expected: 533,
 		},
 	}
+	
+	for tn, td := range tests {
+		t.Run(tn, func(tt *testing.T) {
+			assert.Equal(tt, td.expected, Max(td.input...))
+		})
+	}
+}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			assert.Equal(t, tt.expected, MaxInt(tt.input[0], tt.input[1]))
+func TestMin(t *testing.T) {
+	tests := map[string]struct{
+		input []int
+		expected int
+	}{
+		"no number": {
+			input: []int{},
+			expected: 0,
+		},
+		"one number": {
+			input: []int{1},
+			expected: 1,
+		},
+		"two numbers": {
+			input: []int{1,2},
+			expected: 1,
+		},
+		"more than two numbers": {
+			input: []int{234, 523, 51, 532, 51, 533, 11, 0, 97},
+			expected: 0,
+		},
+	}
+	
+	for tn, td := range tests {
+		t.Run(tn, func(tt *testing.T) {
+			assert.Equal(tt, td.expected, Min(td.input...))
 		})
 	}
 }
